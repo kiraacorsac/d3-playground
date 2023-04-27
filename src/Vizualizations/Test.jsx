2514fa57a9d68768f99be7d37d6a23eb3db90fe2
KiraaCorsac <wire150@gmail.com>
2023-04-27 13:34:29 +0200

    barchart init

--- src/Vizualizations/Test.jsx
+++ src/Vizualizations/Test.jsx
@@ -3,21 +3,37 @@ import { useEffect, useRef } from "react";
 
 export default function Test() {
 
+    const data = [3, 1, 5, 5, 1, 3];
     const svgRef = useRef(null);
+    const innerWidth = window.innerWidth;
+    const innerHeight = window.innerHeight;
+    const shapeWidth = 10
 
     useEffect(() => {
         const canvas = d3
             .select(svgRef.current)
-            .attr("width", 1000)
-            .attr("height", 600)
-        
+            .attr("width", innerWidth)
+            .attr("height", innerHeight)
+
         canvas
             .selectAll("*")
             .remove();
 
-        canvas
-            .append("circle")
-            .attr("r", 100);
+        const rect = canvas
+            .append("g")
+
+        rect.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("x", 10 )
+            .attr("y", 10)
+            .attr("width", shapeWidth)
+            .attr("height", point => point * 10)
+            .attr("fill", "teal")
+            .attr("stroke", "#111")
+            .attr("stroke-width", shapeWidth / 30)
+
+
     }, [svgRef.current])
 
     return <svg ref={svgRef} />;
