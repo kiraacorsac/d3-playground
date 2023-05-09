de61aa6378e61d1248cd8331408d86fd4df1c268
Gaeschlin, Quentin (qg218x) <qg218x@att.com>
2023-05-09 13:34:19 +0200

    diff bars and rotate

--- src/Vizualizations/Test.jsx
+++ src/Vizualizations/Test.jsx
@@ -3,11 +3,12 @@ import { useEffect, useRef } from "react";
 
 export default function Test() {
 
-    const data = [3, 1, 5, 5, 1, 3];
+    const data = [3, 1, 5, 4, 1, 3, 6, 2, 4];
     const svgRef = useRef(null);
     const innerWidth = window.innerWidth;
     const innerHeight = window.innerHeight;
     const shapeWidth = 10
+    const xdelta = 12
 
     useEffect(() => {
         const canvas = d3
@@ -17,21 +18,29 @@ export default function Test() {
 
         canvas
             .selectAll("*")
+
             .remove();
 
         const rect = canvas
             .append("g")
 
+
         rect.selectAll("rect")
-            .data(data)
+            .data(data.entries())
+
             .join("rect")
-            .attr("x", 10 )
+            .attr("x", point => point[0] * xdelta)
             .attr("y", 10)
             .attr("width", shapeWidth)
-            .attr("height", point => point * 10)
+            .attr("height", point => point[1] * 10)
             .attr("fill", "teal")
             .attr("stroke", "#111")
             .attr("stroke-width", shapeWidth / 30)
+            .attr("transform-origin", "center")
+            // .attr("transform", "translate(10 100), scale(1 -1)")
+            // .attr("transform", "scale(1 -1)")
+            .attr("transform", "translate(10 100), scale(1 -1), rotate(-90)")
+
 
 
     }, [svgRef.current])
