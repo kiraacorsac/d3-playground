67e97b6c69cb58ea5941e9a97c823be2383a0773
KiraaCorsac <wire150@gmail.com>
2023-05-16 13:32:21 +0200

    add scale

--- src/Vizualizations/Test.jsx
+++ src/Vizualizations/Test.jsx
@@ -3,11 +3,20 @@ import { useEffect, useRef } from "react";
 
 export default function Test() {
 
-    const data = [3, 1, 5, 5, 1, 3];
+    const data = [-103, -213, 0, 2, 5, -54, 50, 9];
+    const dataAbsolute =  data.map((p) => Math.abs(p))
+    const scaleChart = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 150])
+
+    const testNumber = 1
+
+    console.log(testNumber,scaleChart(testNumber))
+
     const svgRef = useRef(null);
     const innerWidth = window.innerWidth;
     const innerHeight = window.innerHeight;
-    const shapeWidth = 10
+    const shapeWidth = 30
 
     useEffect(() => {
         const canvas = d3
@@ -23,13 +32,13 @@ export default function Test() {
             .append("g")
 
         rect.selectAll("rect")
-            .data(data)
+            .data(dataAbsolute.entries())
             .join("rect")
-            .attr("x", 10 )
-            .attr("y", 10)
+            .attr("x", point => point[0] * 30)
+            .attr("y", 0)
             .attr("width", shapeWidth)
-            .attr("height", point => point * 10)
-            .attr("fill", "teal")
+            .attr("height", point => scaleChart(point[1]))
+            .attr("fill", point => data[point[0]] > 0 ? "green" : "red")
             .attr("stroke", "#111")
             .attr("stroke-width", shapeWidth / 30)
 
