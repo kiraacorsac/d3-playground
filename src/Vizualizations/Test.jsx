e5aee662a087a913698d0986650de53c6d71493f
Filka, Ladislav (lf925u) <lf925u@att.com>
2023-05-16 12:49:52 +0200

    canvas

--- src/Vizualizations/Test.jsx
+++ src/Vizualizations/Test.jsx
@@ -2,39 +2,44 @@ import * as d3 from "d3";
 import { useEffect, useRef } from "react";
 
 export default function Test() {
+  const data = [3, 1, 5, 4, 1, 3, 6, 2, 4];
+  const svgRef = useRef(null);
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  const shapeWidth = 10;
+  const xdelta = 12;
 
-    const data = [3, 1, 5, 5, 1, 3];
-    const svgRef = useRef(null);
-    const innerWidth = window.innerWidth;
-    const innerHeight = window.innerHeight;
-    const shapeWidth = 10
+  useEffect(() => {
+    const canvas = d3
+      .select(svgRef.current)
+      .attr("width", innerWidth)
+      .attr("height", innerHeight);
 
-    useEffect(() => {
-        const canvas = d3
-            .select(svgRef.current)
-            .attr("width", innerWidth)
-            .attr("height", innerHeight)
+    canvas
+      .selectAll("*")
 
-        canvas
-            .selectAll("*")
-            .remove();
+      .remove();
 
-        const rect = canvas
-            .append("g")
+    const rect = canvas.append("g");
 
-        rect.selectAll("rect")
-            .data(data)
-            .join("rect")
-            .attr("x", 10 )
-            .attr("y", 10)
-            .attr("width", shapeWidth)
-            .attr("height", point => point * 10)
-            .attr("fill", "teal")
-            .attr("stroke", "#111")
-            .attr("stroke-width", shapeWidth / 30)
+    rect
+      .selectAll("rect")
+      .data(data.entries())
 
+      .join("rect")
+      .attr("x", (point) => point[0] * xdelta)
+      .attr("y", 10)
+      .attr("width", shapeWidth)
+      .attr("height", (point) => point[1] * 10)
+      .attr("fill", "teal")
+      .attr("stroke", "#111")
+      .attr("stroke-width", shapeWidth / 30)
+      .attr("transform-origin", "center")
+      // .attr("transform", "translate(10 100), scale(1 -1)")
+      // .attr("transform", "scale(1 -1)")
+      .attr("transform", "scale(1 -1),translate(200 100), rotate(5)")
+      // rotate(-90)");
+  }, [svgRef.current]);
 
-    }, [svgRef.current])
-
-    return <svg ref={svgRef} />;
+  return <svg ref={svgRef} />;
 }
