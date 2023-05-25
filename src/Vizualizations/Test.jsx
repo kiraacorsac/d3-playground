e5d9e8746eacea7c52f4da7f6f495083cba17ae1
KiraaCorsac <wire150@gmail.com>
2023-05-25 13:26:58 +0200

    basic chart

--- src/Vizualizations/Test.jsx
+++ src/Vizualizations/Test.jsx
@@ -3,11 +3,13 @@ import { useEffect, useRef } from "react";
 
 export default function Test() {
 
-    const data = [-103, -213, 0, 2, 5, -54, 50, 9];
-    const dataAbsolute =  data.map((p) => Math.abs(p))
+    const data = [-103, -213, 0, 2, 5, -54, 250, 9];
+    const dataAbsolute = data.map((p) => Math.abs(p))
+    const dataMax = d3.max(dataAbsolute);
+    console.log(dataMax)
     const scaleChart = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, 150])
+        .domain([0, dataMax])
+        .range([0, 50])
 
     const testNumber = 1
 
@@ -16,7 +18,7 @@ export default function Test() {
     const svgRef = useRef(null);
     const innerWidth = window.innerWidth;
     const innerHeight = window.innerHeight;
-    const shapeWidth = 30
+    const shapeWidth = 40
 
     useEffect(() => {
         const canvas = d3
@@ -28,21 +30,31 @@ export default function Test() {
             .selectAll("*")
             .remove();
 
-        const rect = canvas
+        const graph = canvas
             .append("g")
+            .attr("transform", "translate(0, 70)")
 
-        rect.selectAll("rect")
+        const bars = graph.selectAll("g")
             .data(dataAbsolute.entries())
-            .join("rect")
-            .attr("x", point => point[0] * 30)
-            .attr("y", 0)
+            .join("g")
+
+        bars.append("rect")
+            .attr("x", point => point[0] * shapeWidth)
+            .attr("y", point => data[point[0]] >= 0 ? -scaleChart(point[1]) : 0)
             .attr("width", shapeWidth)
             .attr("height", point => scaleChart(point[1]))
-            .attr("fill", point => data[point[0]] > 0 ? "green" : "red")
+            .attr("fill", point => data[point[0]] >= 0 ? "green" : "red")
             .attr("stroke", "#111")
             .attr("stroke-width", shapeWidth / 30)
-
-
+        
+        bars.append("text")
+            .attr("x", point => point[0] * shapeWidth + shapeWidth/2)
+            .attr("y", point => data[point[0]] >= 0 ? -scaleChart(point[1]) : scaleChart(point[1]))
+            .attr("text-anchor", "middle")
+            .attr("font-size", 14)
+            .attr("transform", point => data[point[0]] >= 0 ? "translate(0, -2)" : "translate(0, 14)")
+            .text(point => data[point[0]])
+        
     }, [svgRef.current])
 
     return <svg ref={svgRef} />;
